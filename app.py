fb0d9dd5cbce834f958677f3f6bfd3150e8c1696
Ironmat312 <matziarysangelos@gmail.com>
2025-07-14 22:41:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
 import smtplib
 from email.mime.text import MIMEText
 
 def main():
-    # λέξεις-κλειδιά + τοποθεσία
+    # λέξεις-κλειδιά και τοποθεσία
     keywords = ['internal auditor', 'audit assistant', 'financial controller']
     location = 'Athens'
 
-    # χτίζουμε το query string
-    query = '+'.join(keywords + [location])
-    url = f'https://www.indeed.com/jobs?q={query}&l={location}'
+    # χτίζουμε query + το κωδικοποιούμε
+    full_query = ' '.join(keywords + [location])
+    encoded_query = quote_plus(full_query)
+
+    # URL αναζήτησης (παίρνει τις πιο πρόσφατες)
+    # Επιπλέον μπορούμε να προσθέσουμε &fromage=1 για θέσεις 24ωρου:
+    url = f'https://www.indeed.com/jobs?q={encoded_query}&fromage=1'
 
     # αίτημα και parse
     res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(res.text, 'lxml')
 
     # βρίσκουμε τις πρώτες 10 αγγελίες
-    jobs = []
     cards = soup.select('a.tapItem')[:10]
+    jobs = []
     for card in cards:
         title = card.select_one('h2.jobTitle').get_text(strip=True)
         link  = 'https://www.indeed.com' + card['href']
@@ -27,10 +32,7 @@ def main():
         jobs.append(f"{title} @ {company}\n{link}")
 
     # σώμα email
-    if jobs:
-        body = "\n\n".join(jobs)
-    else:
-        body = "Δεν βρέθηκαν νέες αγγελίες σήμερα."
+    body = "\n\n".join(jobs) if jobs else "Δεν βρέθηκαν νέες αγγελίες σήμερα."
 
     # σύνταξη + αποστολή email
     msg = MIMEText(body, _charset='utf-8')
@@ -38,9 +40,9 @@ def main():
     msg['From']    = os.environ['EMAIL_USER']
     msg['To']      = os.environ['EMAIL_USER']
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as s:
-        s.login(os.environ['EMAIL_USER'], os.environ['EMAIL_PASS'])
-        s.send_message(msg)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(os.environ['EMAIL_USER'], os.environ['EMAIL_PASS'])
+        server.send_message(msg)
 
 if __name__ == '__main__':
     main()
